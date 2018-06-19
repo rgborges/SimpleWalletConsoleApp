bbf1d4b8ddde2160008adc7d194f26fb8a243f44
Rafael Borges <rafael.eng.auto@gmail.com>
2018-06-19 09:07:42 -0300

    Projeto iniciado com dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleWalletConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
